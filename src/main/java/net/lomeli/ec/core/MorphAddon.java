b98e2661559b1dd4c6c2d84480216cb0e5270d75
Lomeli12 <anthonylomeli11@gmail.com>
2014-08-04 13:56:56 -0700

    Fixed morph issue

--- src/main/java/net/lomeli/ec/core/MorphAddon.java
+++ src/main/java/net/lomeli/ec/core/MorphAddon.java
@@ -7,18 +7,25 @@ import net.lomeli.ec.entity.*;
 import morph.api.Ability;
 
 public class MorphAddon {
+    private static Ability hostile, fireImunity, climb, waterAllergy, sunBurn;
+
     public static void loadAddon() {
         if (Loader.isModLoaded("Morph"))
             registerAbilities();
     }
 
     private static void registerAbilities() {
-        Ability.mapAbilities(EntityFireCreeper.class, Ability.getNewAbilityFireImmunity(), Ability.getNewAbilityHostile());
-        Ability.mapAbilities(EntityMagmaCreeper.class, Ability.getNewAbilityFireImmunity(), Ability.getNewAbilityHostile(), Ability.getNewAbilityWaterAllergy());
-        Ability.mapAbilities(EntityWaterCreeper.class, Ability.getNewAbilitySwim(true), Ability.getNewAbilityHostile());
-        Ability.mapAbilities(EntityDarkCreeper.class, Ability.getNewAbilitySunburn(), Ability.getNewAbilityHostile());
-        Ability.mapAbilities(EntitySpiderCreeper.class, Ability.getNewAbilityClimb(), Ability.getNewAbilityHostile());
-        Ability.mapAbilities(EntityWindCreeper.class, Ability.getNewAbilityFloat(-0.1141748F, true), Ability.getNewAbilityHostile());
-        Ability.mapAbilities(EntityEnderCreeper.class, Ability.getNewAbilityWaterAllergy(), Ability.getNewAbilityHostile());
+        hostile = Ability.createNewAbilityByType("hostile", null);
+        fireImunity = Ability.createNewAbilityByType("fireImmunity", null);
+        climb = Ability.createNewAbilityByType("climb", null);
+        waterAllergy = Ability.createNewAbilityByType("waterAllergy", null);
+        sunBurn = Ability.createNewAbilityByType("sunburn", null);
+        Ability.mapAbilities(EntityFireCreeper.class, fireImunity, hostile);
+        Ability.mapAbilities(EntityMagmaCreeper.class, fireImunity, hostile, waterAllergy);
+        Ability.mapAbilities(EntityWaterCreeper.class, Ability.createNewAbilityByType("swim", new String[]{"true"}), hostile);
+        Ability.mapAbilities(EntityDarkCreeper.class, sunBurn, hostile);
+        Ability.mapAbilities(EntitySpiderCreeper.class, climb, hostile);
+        Ability.mapAbilities(EntityWindCreeper.class, Ability.createNewAbilityByType("float", new String[]{"-0.1141748F", "true"}), hostile);
+        Ability.mapAbilities(EntityEnderCreeper.class, waterAllergy, hostile);
     }
 }
--- src/main/java/net/lomeli/ec/lib/Strings.java
+++ src/main/java/net/lomeli/ec/lib/Strings.java
@@ -1,7 +1,7 @@
 package net.lomeli.ec.lib;
 
 public class Strings {
-    public static final int MAJOR = 4, MINOR = 2, REVISION = 0;
+    public static final int MAJOR = 4, MINOR = 2, REVISION = 1;
     public static final String VERSION = MAJOR + "." + MINOR + "." + REVISION;
     public static final String MOD_ID = "ElementalCreepers", MOD_NAME = "Elemental Creepers";
     public static final String CORE = "net.lomeli.ec.core", CLIENT = CORE + ".ClientProxy", COMMON = CORE + ".CommonProxy";
